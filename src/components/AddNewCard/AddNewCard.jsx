292bfadf634b9b2e8776782926ea98d906a62a14
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2024-05-02 14:46:08 +0530

    UI updated

--- src/components/AddNewCard/AddNewCard.jsx
+++ src/components/AddNewCard/AddNewCard.jsx
@@ -1,10 +1,27 @@
 import "./AddNewCard.css";
+import {
+    IoIosSearch,
+    IoIosNotificationsOutline,
+    IoIosInformationCircleOutline,
+} from "react-icons/io";
+import { CiUser } from "react-icons/ci";
 
 const AddNewCard = ({ setShowModal }) => {
     return (
         <nav>
-            <span>Planner board</span>
-            <button onClick={() => setShowModal(true)}>add card</button>
+            <div className="left-section">
+                <span>Planner board</span>
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." />
+                    <IoIosSearch />
+                </div>
+            </div>
+            <div className="right-section">
+                <button onClick={() => setShowModal(true)}>add card</button>
+                <IoIosNotificationsOutline />
+                <IoIosInformationCircleOutline />
+                <CiUser />
+            </div>
         </nav>
     );
 };
